b0e5af7d07d73fbc2c4de743656bc757e03dcc46
Alex Tumwesigye <atumwesigye@gmail.com>
2025-01-31 14:47:52 +0300

    Add pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,17 @@ module.exports = {
     apps: [
       {
         name: "dev-app",
-        script: "pnpm dev:app"
+        script: "pnpm dev:app",
+        instances: 5, // Number of instances to run (e.g., scale to multiple CPUs)
+        autorestart: true, // Restart on crash or failure
+        watch: false, // Enable to restart on file changes
+        max_memory_restart: "1G", // Restart if memory usage exceeds 1GB
+        env: {
+          NODE_ENV: "development",
+        },
+        env_production: {
+          NODE_ENV: "production",
+        },
       }
     ],
   };
