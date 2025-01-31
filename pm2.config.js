266285579635ab87505801bef870d7c0cd4e59b9
Alex Tumwesigye <atumwesigye@gmail.com>
2025-01-31 14:39:35 +0300

    Add pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,12 +1,6 @@
 module.exports = {
     apps: [
       {
-        name: "dev-watch",
-        script: "tsx",
-        args: "watch src/index.ts",
-        interpreter: "node",
-      },
-      {
         name: "dev-app",
         script: "pnpm dev:app"
       }
